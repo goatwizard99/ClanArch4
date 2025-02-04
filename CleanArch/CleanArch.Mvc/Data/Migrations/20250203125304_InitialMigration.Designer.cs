e3bb2a28292ecb78174fbdc3fce04dfe8446ef7a
goatwizard99 <wizardfool@yahoo.comm>
2025-02-04 07:25:51 -0500

    fixed

--- CleanArch/CleanArch.Mvc/Data/Migrations/20250203125304_InitialMigration.Designer.cs
+++ CleanArch/CleanArch.Mvc/Data/Migrations/20250203125304_InitialMigration.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArch.Mvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190423034954_InitialMigration")]
+    [Migration("20250203125304_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.1-servicing-10028")
+                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
